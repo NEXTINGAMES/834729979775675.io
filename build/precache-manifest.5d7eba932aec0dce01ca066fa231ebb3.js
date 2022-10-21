453ffe996f136768310551ad83df079413b7851c
nextingames <nextingames111@gmail.com>
2022-10-21 11:08:23 +0530

    build

---
+++ build/precache-manifest.5d7eba932aec0dce01ca066fa231ebb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aad45e37c4d795d611d6bb01533d7a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6edca9bf2713fdadd79b",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "6edca9bf2713fdadd79b",
+    "url": "/static/js/2.183b5ea3.chunk.js"
+  },
+  {
+    "revision": "18747109ff94a5e339e75358c2bc0a91",
+    "url": "/static/js/2.183b5ea3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8434b373999e8ae17fa",
+    "url": "/static/js/main.40ce81dd.chunk.js"
+  },
+  {
+    "revision": "992c1d3a7787c5ab29a5",
+    "url": "/static/js/runtime-main.eeaa320a.js"
+  }
+]);
